151d830666ae47454d1048a3cf22865a6daac91d
Вадим Павличенко <Vadymchyk111@gmail.com>
2022-05-27 16:20:24 +0300

    FixedCode

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -10,9 +10,10 @@ namespace Task6
     {
         static void Main(string[] args)
         {
-            int[] images = new int[52];
-            int rest = images.Length % 3;
-            int countOfRows = (images.Length - rest) / 3;
+            int countOfImages = 52;
+            int imagesInRaw=3;
+            int rest = countOfImages % imagesInRaw;
+            int countOfRows = (countOfImages - rest) / imagesInRaw;
 
             Console.WriteLine("Вы можете вывести " + countOfRows + " строк");
             Console.WriteLine("Остаток = " + rest);
